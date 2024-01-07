334cba7dd4708a1db740cf021a0d1b843cb4a23d
AnnBortnik <32511837+AnnBortnik@users.noreply.github.com>
2024-01-07 19:38:41 +0100

    Initial commit

---
+++ src/components/notification/Notification.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const CenterText = styled.h2`
+    text-align: center;
+`;
+
+const Notification = ({ message }) => (
+    <CenterText>{message}</CenterText>
+);
+
+export default Notification;
